76b50bd40afa606c4d61a62a1a81f33db69b3e36
Tamoor <vulpeeze@gmail.com>
2024-03-16 23:07:04 +0000

    Adding gallery uploader code

---
+++ code.js
@@ -0,0 +1,33 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function uploadImageSet(imageNum){
+  list = new DataTransfer();
+  f = document.querySelector("#imagefileHolder").files[imageNum];
+  list.items.add(f);
+
+  myFileList = list.files;
+  document.querySelector("#imagefile").files = myFileList;
+  uploadImage();
+}
+
+async function uploadImages() {
+  fl = document.querySelector("#imagefileHolder").files.length;
+  for (let i = 0; i < fl; i++) {
+    console.log("Image " + i);
+    uploadImageSet(i);
+    await sleep(2000);
+  }
+}
+
+document.querySelector("#CharacterAddImageSection").innerHTML = `
+<label for="imagefile">Select multiple files</label>
+<input id="imagefileHolder" type="file" multiple="multiple" accept="image/jpeg, image/png, image/jpg">
+<input type="file" id="imagefile" disabled hidden>
+<input id=\"addimagebutton\" type=\"button\" onclick=\"uploadImage(); return false;\" value=\"Add Image\" disabled hidden>
+<input id=\"uploadimagesbutton\" type=\"button\" return false;\" value=\"Upload Images\">
+`;
+document.querySelector("#uploadimagesbutton").addEventListener('click', function(e){
+  return uploadImages();
+})
---
+++ kink Automation.js
@@ -0,0 +1,60 @@
+document.getElementById('description').insertAdjacentHTML("beforebegin", "<div id='kinkAutomationLabels'>")
+
+document.querySelector('#kinkAutomationLabels').innerHTML = "<h3>Custom Kink Names</h3>\
+<table>\
+<form>\
+<tr><td><label for='fave'>Fave: </label>    </td><td><input type='text' id='fave' name='fave'></input></td></tr>\
+<tr><td><label for='yes'>Yes: </label>      </td><td><input type='text' id='yes' name='yes'></input></td></tr>\
+<tr><td><label for='maybe'>Maybe: </label>  </td><td><input type='text' id='maybe' name='maybe'></input></td></tr>\
+<tr><td><label for='no'>No: </label>        </td><td><input type='text' id='no' name='no'></input></td></tr>\
+<tr><td><button onclick='organiseKinks()'>Click here to organise kinks</button></td></tr>\
+</form></table>"
+
+function kinkSelect(category){
+  kinks = document.querySelector('#kinks')
+  kl = kinks.length
+  for(let i=0; i<kl;i++){
+    if(kinks[i].classList==category){
+      kinks[i].selected = true
+    }
+  }
+}
+
+function ckinkSelect(text){
+  ck = document.querySelector('#customKinks')
+  ckl = ck.length
+  for(let i=0;i<ckl;i++){
+    if(ck[i].innerText == text){
+      document.querySelector('#customKinks')[i].selected = true
+    }
+  }
+}
+
+function kinksPlacer(categories, kinkText){
+  for(ii=0;ii<4;ii++){
+    kinkSelect(categories[ii])
+    ckinkSelect(kinkText[ii])
+    document.querySelector('#addKink').click()
+  }
+}
+
+function textPicker(){
+  faveText = document.querySelector("#fave").value
+  yesText = document.querySelector("#yes").value
+  maybeText = document.querySelector("#maybe").value
+  noText = document.querySelector("#no").value
+  return [faveText, yesText, maybeText, noText]
+}
+
+function organiseKinks(){
+  categories = ["fave", "yes", "maybe", "no"]
+  kinkText = textPicker()
+  groupCount = document.querySelector("#groups").children[1].childElementCount
+
+  for(j=0;j<groupCount;j++){
+    document.querySelector('#groups').children[1][j].selected = true
+    FList.Subfetish.Ui.populateKinks();
+    FList.Subfetish.Ui.Lists.Cache.recache('groups');
+    kinksPlacer(categories, kinkText)
+	}
+}
