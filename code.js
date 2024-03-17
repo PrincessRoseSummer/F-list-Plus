14db9decbf200bb7008a060b40daaf96eb36cbf0
Tamoor <vulpeeze@gmail.com>
2024-03-17 11:06:29 +0000

    Image validation implemented

--- code.js
+++ code.js
@@ -9,6 +9,7 @@ function uploadImageSet(imageNum){
 
   myFileList = list.files;
   document.querySelector("#imagefile").files = myFileList;
+  is_image_valid(f);
   uploadImage();
 }
 
@@ -16,10 +17,55 @@ async function uploadImages() {
   fl = document.querySelector("#imagefileHolder").files.length;
   disableButton();
   for (let i = 0; i < fl; i++) {
-    console.log("Image " + i);
     uploadImageSet(i);
     await sleep(2000);
   }
+}
+
+const is_image_valid = (file) => {
+  var s = file.size/1024/1024;
+  let reader = new FileReader();    // create a file reader instance.
+
+  reader.onload = function (e) {
+    let img = new Image();
+    img.src = e.target.result;
+
+    img.onload = function () {
+      var w = this.width;
+      var h = this.height;
+      if (h > 4000 || w > 4000 || s>2) {
+        invalidImageLister(file, h, w, s)
+      }
+    }
+  };
+  reader.readAsDataURL(file);
+}
+
+function invalidImageLister(file, height, width, size) {
+   if (document.getElementById("invalidImageList")==undefined) {
+    var title = document.createElement("p");
+    title.innerText = "Invalid images";
+    title.style.textDecorationLine = "underline";
+    title.style.padding = "20px 20px 0 20px"
+    title.id = "invalidImageList";
+    document.querySelector("#uploadimagesbutton").after(title);
+
+    var list = document.createElement("ul");
+    list.id = "invalidList";
+    document.querySelector("#invalidImageList").after(list);
+   }
+   size = String(size).slice(0, 5)
+   if (height>4000) {
+    height = `<span style="color: red;">` + height + `</span>`;
+   }
+   if (width>4000) {
+    width = `<span style="color: red;">` + width + `</span>`;
+   }
+   if (size>2) {
+    size = `<span style="color: red;">` + size + `mb</span>`;
+   }
+   invalidFileListing = `<li style="padding: 5px 20px;">` + file.name + `: ` + height + `x` + width + ` ` + size + `</li>`;
+   document.querySelector("#invalidList").innerHTML += invalidFileListing;
 }
 
 async function disableButton(){
